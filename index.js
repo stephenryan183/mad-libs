daf1faee7c69d6a966d955b282f0ec6f8d82343d
stephenryan183 <stephenryan183@gmail.com>
2022-12-24 23:02:15 -0800

    local-and-global-scope

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ ourFunctionwithArgs(10,5);  //when completed will output 5
 
 //Global scope varables are variables that are set outside of a function and will run code inside any function below it
 //Local scope variables are ties down to a set function they are set inside of
+//It is Posiible to have a the same global and local variable  but the local variable will take presedence
+
+//below is an example of both a local and global variable that are the same
+
+var outerWear = "T-Shirt";
+function myOutfit() {
+    var outerWear = "sweater";
+    return outerWear;
+}
+console.log(myOutfit());
+console.log(outerWear);  //When console logged sweater will come first then T Shirt fill follow after
